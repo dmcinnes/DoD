c165edf405758fa4bfacb4ddbee7a20ea2a9572c
Doug McInnes <doug@dougmcinnes.com>
2016-01-02 21:00:22 -0800

    start of a hand

--- game.js
+++ game.js
@@ -3,18 +3,22 @@ $(function () {
   var game = $('#game');
 
   game.find('.card').draggable({
-    revert: true
+    revert: true,
+    connectToSortable: ".stack.wide"
   });
   game.find('.stack').droppable({
     hoverClass: 'stack-hover',
     drop: function (event, ui) {
-      console.log(ui);
       // move to the new stack
       var card = $(ui.draggable).detach();
-      // so it doesn't 'revert' after being added to this stack
-      card.css({left: 0, top: 0});
+      // so it doesn't 'revert' after being added to a non-sortable stack
+      if (card.css('position') == 'relative') {
+        card.css({left: 0, top: 0});
+      }
       $(this).prepend(card);
     }
+  });
+  game.find('.stack.wide').sortable({
   });
 
   game.on('click', '.card', function (event) {
